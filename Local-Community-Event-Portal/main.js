69bdd8fdb5335ad06c6a59cac442322fc91510ac
Hemapriya-10 <priyamarane105@gmail.com>
2026-05-30 13:44:57 +0530

    Completed JavaScript Exercise 1 - Basics and Setup

---
+++ Local-Community-Event-Portal/main.js
@@ -0,0 +1,11 @@
+// =====================================
+// JavaScript Basics & Setup
+// =====================================
+
+console.log("Welcome to the Community Portal");
+
+window.onload = function () {
+
+    alert("Welcome to the Community Portal");
+
+};
